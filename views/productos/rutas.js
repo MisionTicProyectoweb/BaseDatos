19f60a9cf7514a680d6dd0ea3f795b4a84ef3587
mreyesq <marcereyesq@gmail.com>
2021-10-10 18:59:35 -0500

    Ajuste de textos

--- views/productos/rutas.js
+++ views/productos/rutas.js
@@ -18,7 +18,7 @@ const genercCallback = (res) => (err, result) => {
 };
 
 rutasProducto.route('/productos').get((req, res) => {
-  console.log('alguien hizo get en la ruta /productos');
+  console.log('Alguien hizo get en la ruta /productos');
   queryAllProductos(genercCallback(res));
 });
 
@@ -27,7 +27,7 @@ rutasProducto.route('/productos/nuevo').post((req, res) => {
 });
 
 rutasProducto.route('/productos/:id').get((req, res) => {
-  console.log('alguien hizo get en la ruta /productos');
+  console.log('Alguien hizo get en la ruta /productos');
   consultarProducto(req.params.id, genercCallback(res));
 });
 
