b0f1c921489da3ac7c3646807b44c97b5f1df307
manuel <manuelguma25@gmail.com>
2021-10-15 23:03:28 -0500

    agregar jwtcheck para asegurar las rutas

--- server.js
+++ server.js
@@ -5,21 +5,36 @@ import { conectarBD } from './db/db.js';
 import rutasProducto from './views/productos/rutas.js';
 import rutasUsuario from './views/usuarios/rutas.js';
 import rutasVenta from './views/ventas/rutas.js';
-
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
 dotenv.config({ path: './.env' });
-
 const app = Express();
-
 app.use(Express.json());
 app.use(Cors());
+
+
+
+const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://mintic-proyecto.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'api-autenticacion-almacen-mintic',
+    issuer: 'https://mintic-proyecto.us.auth0.com/',
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 app.use(rutasProducto);
 app.use(rutasUsuario);
 app.use(rutasVenta);
-
 const main = () => {
-  return app.listen(process.env.PORT, () => {
-    console.log(`escuchando puerto ${process.env.PORT}`);
-  });
+    return app.listen(process.env.PORT, () => {
+        console.log(`escuchando puerto ${process.env.PORT}`);
+    });
 };
+
 
 conectarBD(main);
